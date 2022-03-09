199867803970b6b6435dda65259be4b11c218393
Vansh kamboj <vansh.kamboj@smartdatainc.net>
2022-03-09 14:06:50 +0530

    fix part module week issue and freen screen issue

--- src/App.web.js
+++ src/App.web.js
@@ -6,18 +6,23 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import WebRouter from './config/webRouter';
 import style from './assets/scss/global.scss';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 //import store from './store/setup';
 import * as AppActions from '../src/actions';
-import setup from "./store/setup.web";
+import setup from './store/setup.web';
 const store = setup();
 
 const App = () => {
-  console.log(store, "my store");
-  store.dispatch(AppActions.getQuoteData(res => { }));
+  useEffect(() => {
+    window.process = {
+      ...window.process,
+    };
+  }, []);
+  console.log(store, 'my store');
+  store.dispatch(AppActions.getQuoteData((res) => {}));
   return (
     <Provider store={store}>
       <WebRouter />
