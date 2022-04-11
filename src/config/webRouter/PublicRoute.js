24a88eb5168d09f659051a282b60dddbd725b414
priyak-sdei <priya.kumari@smartdatainc.net>
2022-04-11 13:13:59 +0530

    aliase added config

--- src/config/webRouter/PublicRoute.js
+++ src/config/webRouter/PublicRoute.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { getItem } from '../../utils/AsyncUtils';
-import { useDispatch, useSelector } from 'react-redux';
+import {Route, Redirect} from 'react-router-dom';
+import {getItem} from '@utils/AsyncUtils';
 
-const epdsAssesment = getItem('epdsAssesment')
-const PublicRoute = ({ component: Component, restricted, ...rest }) => {
+const PublicRoute = ({component: Component, restricted, ...rest}) => {
   const isLogin = getItem('token') ? true : false;
   return (
     // restricted = false meaning public route
