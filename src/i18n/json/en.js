ed3e42d1d6cd25cd5f159db84355d436c476b276
Vansh kamboj <vansh.kamboj@smartdatainc.net>
2022-07-18 17:59:05 +0530

    validation msg add in language translation

--- src/i18n/json/en.js
+++ src/i18n/json/en.js
@@ -116,6 +116,21 @@ export default {
       "FBLoginFail": "Facebook Login fails! Unable to access user's email",
       "GoogleRegistered": "This user is currently not registered with Mamalift. Please register at",
       "AppleLoginCancel": "Apple login cancelled.",
+      "CURRENT_PWD_REQ": 'Please enter your current password.',
+      "NEW_PWD_REQ": 'Please enter your new password.',
+      "C_PWD_REQ": 'Please enter your confirm new password.',
+      "PWD_ERR": 'Current password and new password can not be same.',
+      "passowrd_require": "Password is required.",
+      "PASSWORD_ERROR": "Minimum 8 character length, atleast 1 special character and 1 digit.",
+      "PASSWORD_NOT_MATCH": "New password and confirm password does not match.",
+      "EnterPhone": "Please enter phone number",
+      "ValidPhone": "Please enter a valid phone number",
+      "EnterCity": "Please enter a city",
+      "ValidCity": "Please enter valid city",
+      "EnterZip": "Please enter zipcode",
+      "ValidZip": "Please enter a valid zipcode",
+
+
 
       "SEND": "Send",
       "SUBJECT": "Subject",
